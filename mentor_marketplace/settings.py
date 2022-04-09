91f830f6282f8fa83664fe7a3706082be19871bd
elgas <elgasmaci@gmail.com>
2022-04-09 08:48:38 +0000

    Removed secret key and set debug

--- mentor_marketplace/settings.py
+++ mentor_marketplace/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q&v371$iyr1-lb_exmk6jnutwx+g*t9w_hgl&+4fm5m$ltu2&k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
